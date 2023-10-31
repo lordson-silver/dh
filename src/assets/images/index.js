af715be5b2ca9a29d7d5c145c27649e8708eb938
lordson-silver <lordsonochuko5@gmail.com>
2023-10-31 07:45:43 +0000

    Fix: bug fix

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,5 @@ import image2 from './image2.png';
 import image3 from './image3.png';
 import image4 from './image4.png';
 import image5 from './image5.png';
-import landing from './landing.png';
 
-export { image2, image3, image4, image5, landing };
+export { image2, image3, image4, image5 };
